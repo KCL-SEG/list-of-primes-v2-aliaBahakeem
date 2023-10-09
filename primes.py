5671a70d6a6a478bf046ccad0c6efc840602352d
Alia Raed <alia.bahakeem@kcl.ac.uk>
2023-10-09 19:25:45 +0100

    add solutions to primes.py

--- primes.py
+++ primes.py
@@ -4,3 +4,35 @@
 def primes(number_of_primes):
     list = []
     return list
+    def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Input must be a positive integer")
+    
+    prime_list = []
+    num = 2  # Start with the first prime number
+    
+    while len(prime_list) < number_of_primes:
+        if is_prime(num):
+            prime_list.append(num)
+        num += 1
+    
+    return prime_list
+
+# Test cases
+try:
+    result1 = primes(10)
+    print(result1)  # Output: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
