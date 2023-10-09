02d4ceb1bfe28c1dbddbe47d498c4f39f6ad92ac
Alia Raed <alia.bahakeem@kcl.ac.uk>
2023-10-09 19:33:22 +0100

    add solutions to primes.py

--- primes.py
+++ primes.py
@@ -4,19 +4,20 @@
 def primes(number_of_primes):
     list = []
     return list
-    def is_prime(n):
-        if n <= 1:
-            return False
-        elif n <= 3:
-            return True
-        elif n % 2 == 0 or n % 3 == 0:
-            return False
-        i = 5
-        while i * i <= n:
-            if n % i == 0 or n % (i + 2) == 0:
-                return False
-            i += 6
+    
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
         return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 def primes(number_of_primes):
     if number_of_primes <= 0:
@@ -33,6 +34,6 @@ def primes(number_of_primes):
     return prime_list
 
 # Test cases
-try:
-    result1 = primes(10)
-    print(result1)  # Output: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+result1 = primes(10)
+print(result1)  # Output: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
